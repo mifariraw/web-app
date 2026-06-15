e508a02084190d87381524cb66a93266023e800e
AlinAlexandruPeter <88247116+AlinAlexandruPeter@users.noreply.github.com>
2026-06-16 02:37:52 +0300

    Update page.tsx

--- src/app/[locale]/events/[type]/page.tsx
+++ src/app/[locale]/events/[type]/page.tsx
@@ -6,7 +6,7 @@ interface PageProps {
 }
 
 const EventPage = async ({ params }: PageProps) => {
-  const acceptedTypes = ['concert', 'event', 'personal_project', 'portraits']
+  const acceptedTypes = ['concert', 'event', 'personal_projects', 'portraits']
   const { type } = await params
 
   if (!acceptedTypes.includes(type)) {
